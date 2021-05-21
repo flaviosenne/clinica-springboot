dd0345db4e8516320b170104a99625cd035cc6c9
Joao flavio <flaviosenne123@gmail.com>
2021-05-21 14:59:21 -0300

    setup project

---
+++ consulta/src/main/java/com/curso/security/consulta/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.curso.security.consulta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+	// abrir pagina home
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}	
+}
