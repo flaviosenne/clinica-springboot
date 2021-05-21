81d1c584befce105195fc87be3774034a39f661f
Joao flavio <flaviosenne123@gmail.com>
2021-05-21 16:05:18 -0300

    config spring security

--- consulta/src/main/java/com/curso/security/consulta/controller/HomeController.java
+++ consulta/src/main/java/com/curso/security/consulta/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	// abrir pagina home
-	@GetMapping("/")
+	@GetMapping({"/", "/home"})
 	public String home() {
 		return "home";
 	}	
