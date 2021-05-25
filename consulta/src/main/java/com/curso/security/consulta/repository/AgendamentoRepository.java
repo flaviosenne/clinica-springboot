d53a84d114b592f97793dbfba9d5630a2d062c9d
Joao flavio <flaviosenne123@gmail.com>
2021-05-25 16:49:10 -0300

    get horario in consulta

--- consulta/src/main/java/com/curso/security/consulta/repository/AgendamentoRepository.java
+++ consulta/src/main/java/com/curso/security/consulta/repository/AgendamentoRepository.java
@@ -1,7 +1,25 @@
 package com.curso.security.consulta.repository;
 
 import com.curso.security.consulta.domain.Agendamento;
+import com.curso.security.consulta.domain.Horario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+    @Query("select h " +
+            "from Horario h " +
+            "where not exists(" +
+                " select a.horario.id " +
+                " from Agendamento a " +
+                " where " +
+                    " a.medico.id = :id " +
+                    " and a.dataConsulta = :data " +
+                    " and a.horario.id = h.id" +
+            ") " +
+            " order by h.horaMinuto asc")
+    List<Horario> findByMedicoIdAndDataNotHorarioAgendado(Long id, LocalDate data);
 }
--- consulta/src/main/java/com/curso/security/consulta/service/AgendamentoService.java
+++ consulta/src/main/java/com/curso/security/consulta/service/AgendamentoService.java
@@ -1,7 +1,21 @@
 package com.curso.security.consulta.service;
 
+import com.curso.security.consulta.domain.Horario;
+import com.curso.security.consulta.repository.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class AgendamentoService {
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    @Transactional
+    public List<Horario> buscarHorariosNaoAgendadosPorMedicoIdEData(Long id, LocalDate data) {
+        return agendamentoRepository.findByMedicoIdAndDataNotHorarioAgendado(id, data);
+    }
 }
