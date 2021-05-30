e5b6797ca0a8bbb67c37d4b952ee6b25d0f96dda
Joao flavio <flaviosenne123@gmail.com>
2021-05-30 09:41:16 -0300

    authority admin and paciente

--- consulta/src/main/java/com/curso/security/consulta/controller/AgendamentoController.java
+++ consulta/src/main/java/com/curso/security/consulta/controller/AgendamentoController.java
@@ -10,6 +10,7 @@ import com.curso.security.consulta.service.EspecialidadesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -40,11 +41,14 @@ public class AgendamentoController {
     private EspecialidadesService especialidadeService;
 
 
+    @PreAuthorize("hasAnyAuthority('PACIENTE','MEDICO')")
     @GetMapping({"/agendar"})
     public String agendarConsulta(Agendamento agendamento){
         return "agendamento/cadastro";
     }
 
+
+    @PreAuthorize("hasAnyAuthority('PACIENTE','MEDICO')")
     @GetMapping("/horario/medico/{id}/data/{data}")
     public ResponseEntity<?> getHorarios(@PathVariable("id")Long id,
                                          @PathVariable("data") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data ){
@@ -52,6 +56,8 @@ public class AgendamentoController {
         return ResponseEntity.ok(agendamentoService.buscarHorariosNaoAgendadosPorMedicoIdEData(id, data));
     }
 
+
+    @PreAuthorize("hasAuthority('PACIENTE')")
     @PostMapping({"/salvar"})
     public String salvar(Agendamento agendamento, RedirectAttributes redirect, @AuthenticationPrincipal UserDetails user){
         Paciente paciente = pacienteService.buscarPorUsuarioEmail(user.getUsername());
@@ -69,12 +75,16 @@ public class AgendamentoController {
         return "redirect:/agendamentos/agendar";
     }
 
+
+    @PreAuthorize("hasAnyAuthority('PACIENTE','MEDICO')")
     @GetMapping({"/historico/paciente","historico/consultas"})
     public String historico(){
 
         return "agendamento/historico-paciente";
     }
 
+
+    @PreAuthorize("hasAnyAuthority('PACIENTE','MEDICO')")
     @GetMapping("/datatables/server/historico")
     public ResponseEntity<?> historicoAgendamentoPorPaciente(HttpServletRequest request, @AuthenticationPrincipal UserDetails user){
 
@@ -87,6 +97,8 @@ public class AgendamentoController {
         return ResponseEntity.notFound().build();
     }
 
+
+    @PreAuthorize("hasAnyAuthority('PACIENTE','MEDICO')")
     @GetMapping("/editar/consulta/{id}")
     public String preEditarConsultaPaciente(@PathVariable("id") Long id,
                                             ModelMap model,
@@ -97,6 +109,8 @@ public class AgendamentoController {
         return "agendamento/cadastro";
     }
 
+
+    @PreAuthorize("hasAnyAuthority('PACIENTE','MEDICO')")
     @Transactional
     @PostMapping("/editar")
     public String editarConsulta(Agendamento agendamento, RedirectAttributes redirect, @AuthenticationPrincipal User user){
@@ -112,6 +126,8 @@ public class AgendamentoController {
 
     }
 
+
+    @PreAuthorize("hasAuthority('PACIENTE')")
     @GetMapping("/excluir/consulta/{id}")
     public String excluirConsulta(@PathVariable("id") Long id, RedirectAttributes redirect){
         agendamentoService.remover(id);
--- consulta/src/main/java/com/curso/security/consulta/security/SecurityConfig.java
+++ consulta/src/main/java/com/curso/security/consulta/security/SecurityConfig.java
@@ -4,11 +4,13 @@ import com.curso.security.consulta.domain.PerfilTipo;
 import com.curso.security.consulta.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
