ad53a5729fea4d73d267ce25b50589d9edfae485
Joao flavio <flaviosenne123@gmail.com>
2021-06-01 15:43:41 -0300

    send email test

--- consulta/src/main/java/com/curso/security/consulta/ConsultaApplication.java
+++ consulta/src/main/java/com/curso/security/consulta/ConsultaApplication.java
@@ -1,15 +1,31 @@
 package com.curso.security.consulta;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class ConsultaApplication {
+public class ConsultaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 //		System.out.println(new BCryptPasswordEncoder().encode("12345"));
 		SpringApplication.run(ConsultaApplication.class, args);
 	}
 
+	@Autowired
+	JavaMailSender sender;
+
+	@Override
+	public void run(String... args) throws Exception {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("joaodev3@gmail.com");
+		message.setText("test 1");
+		message.setSubject("test 1");
+
+		sender.send(message);
+	}
 }
